d5f665f8f77c250f4812d98e8c3878e4404eb25a
kira-0521 <kiratanaka0521@gmail.com>
2021-12-05 00:12:58 +0900

    ブログ詳細のスタイル記述

--- pages/blog/[id]/index.vue
+++ pages/blog/[id]/index.vue
@@ -3,55 +3,154 @@ import { ResData } from '../../../composables/types/api/microcmsResponse';
 
 const route = useRoute();
 const { data: article } = await useFetch<string, ResData>(
-  `/engineer-blog/${route.params.id}?richEditorFormat=object`,
+  `/engineer-blog/${route.params.id}`,
   {
     baseURL: 'https://kira-engineer.microcms.io/api/v1/',
-    pick: ['id', 'title', 'content', 'createdAt', 'updatedAt'],
+    pick: [
+      'id',
+      'title',
+      'content',
+      'createdAt',
+      'updatedAt',
+      'publishedAt',
+      'image',
+    ],
     headers: {
       'X-MICROCMS-API-KEY': '2cd222d7e07842e291f7bfae11fe641d559e',
     },
   }
 );
-console.log(article.value.content.contents);
+console.log(article.value);
 </script>
 
 <template>
   <div class="bg-gray-100 pb-6 sm:pb-8 lg:pb-12">
     <div class="max-w-screen-xl px-4 md:px-8 mx-auto">
       <HeaderLayout />
-      <div class="py-6 lg:container mx-auto">
-        <!-- <div class="grid grid-cols-3 gap-4 py-6 lg:container mx-auto"> -->
+      <!-- <div class="py-6 lg:container mx-auto"> -->
+      <div class="grid grid-cols-3 gap-4 py-6 lg:container mx-auto">
         <div class="col-span-2 bg-white rounded-md shadow">
-          <div class="px-6 py-4 sm:px-8 sm:py-6 lg:px-12 lg:py-10">
-            <div class="px-4 md:px-8">
-              <h1
-                class="text-gray-800 text-2xl sm:text-3xl font-bold text-center mb-4 md:mb-6"
-              >
-                {{ article.title }}
-              </h1>
+          <div class="px-6 py-6 sm:px-8 sm:py-8 lg:px-12 lg:py-10">
+            <h1
+              class="text-gray-800 text-2xl sm:text-3xl font-bold text-center"
+            >
+              {{ article.title }}
+            </h1>
 
+            <div class="mt-8 w-full mx-auto">
+              <p class="publishedAt text-gray-400">
+                公開日:
+                <time :datetime="article.publishedAt">{{
+                  new Date(Date.parse(article.publishedAt)).toLocaleDateString()
+                }}</time>
+              </p>
               <div
-                class="bg-gray-100 overflow-hidden rounded-lg shadow-lg relative mb-6 md:mb-8 w-2/3 h-2/4 mx-auto"
+                class="bg-gray-100 overflow-hidden rounded-lg shadow-lg relative mb-6 md:mb-8 w-full h-2/4"
               >
                 <img
-                  :src="article.content.contents[0].value"
+                  :src="article.image.url"
                   loading="lazy"
                   alt="Photo by Minh Pham"
                   class="w-full h-full object-cover object-center"
                 />
               </div>
 
-              <div class="w-2/3 mx-auto">
-                <p class="text-gray-500 sm:text-lg mb-6 md:mb-8">
-                  {{ article.content.contents[1].value[1].value }}
-                </p>
+              <div class="w-full">
+                <div class="articles" v-html="article.content"></div>
               </div>
             </div>
           </div>
         </div>
-        <!-- <div class="col-span-1 bg-white rounded-md shadow"></div> -->
+        <div class="col-span-1 bg-white rounded-md shadow"></div>
       </div>
       <FooterLayout />
     </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+:deep(.articles) {
+  letter-spacing: 0.5px;
+
+  > h2 {
+    font-size: 24px;
+    font-weight: bold;
+    color: #66a1ea;
+    margin: 40px 0 16px;
+    border-bottom: 1px solid #ddd;
+  }
+
+  > h3 {
+    font-size: 20px;
+    font-weight: bold;
+    color: #2d3748;
+    margin: 30px 0 15px 5px;
+  }
+
+  > ul {
+    list-style: circle;
+    list-style-position: inside;
+  }
+
+  > ol {
+    list-style: decimal;
+    list-style-position: inside;
+  }
+
+  > p {
+    line-height: 1.8;
+    letter-spacing: 0.5px;
+
+    > a {
+      color: #0000ee;
+    }
+
+    > span {
+      color: #a0aec0;
+    }
+
+    > img {
+      border-radius: 0.5rem;
+      width: 100%;
+      overflow: hidden;
+      box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1),
+        0 4px 6px -2px rgba(0, 0, 0, 0.05);
+    }
+  }
+
+  > pre {
+    > code {
+      display: block;
+      padding: 1.1em 1.25em; /* 文字周りの余白 */
+      color: #444; /* 文字色 */
+      background-color: #e7edf3; /* 背景色 */
+      border-radius: 3px; /* 角丸 */
+      border: solid 1px #d6dde4; /* 枠線 */
+    }
+  }
+
+  > blockquote {
+    margin-top: 45px;
+    position: relative;
+    padding: 35px 15px 10px 15px;
+    box-sizing: border-box;
+    font-style: italic;
+    background: #f5f5f5;
+    color: #777777;
+    border-left: 4px solid #a3bffa;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.14);
+
+    &::before {
+      display: inline-block;
+      position: absolute;
+      top: 5px;
+      left: 3px;
+      content: '“';
+      font-family: sans-serif;
+      color: #a3bffa;
+      font-size: 90px;
+      line-height: 1;
+    }
+  }
+}
+</style>
