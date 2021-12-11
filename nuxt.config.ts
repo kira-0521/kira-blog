f486db713e42eba8ef3d2b23dde776cf29d5831b
kira-0521 <kiratanaka0521@gmail.com>
2021-12-11 22:22:42 +0900

    faviconとタイトル追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,16 @@ const { BASE_URL, API_KEY } = process.env;
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
   target: 'static',
+  head: {
+    titleTemplate: 'Terakoya',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+
+      // hid is used as unique identifier. Do not use `vmid` for it as it will not work
+      { hid: 'description', name: 'description', content: 'Meta description' },
+    ],
+  },
   publicRuntimeConfig: {
     baseURL: BASE_URL,
     apiKey: API_KEY,
