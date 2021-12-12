ce06f6b8d290660329f7396537c4a63d827fb5e8
kira-0521 <kiratanaka0521@gmail.com>
2021-12-12 11:41:05 +0900

    head設定変更

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,17 +3,14 @@ const { BASE_URL, API_KEY } = process.env;
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-  target: 'static',
   head: {
     titleTemplate: 'Terakoya',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-
-      // hid is used as unique identifier. Do not use `vmid` for it as it will not work
       { hid: 'description', name: 'description', content: 'Meta description' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{ rel: 'favicon', href: 'favicon.ico' }],
   },
   publicRuntimeConfig: {
     baseURL: BASE_URL,
