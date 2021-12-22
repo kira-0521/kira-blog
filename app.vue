7ab8ec08cf2a8b69f62d2149ff4cf3c209560be7
kira-0521 <kiratanaka0521@gmail.com>
2021-12-22 09:38:58 +0900

    headタグ作成

--- app.vue
+++ app.vue
@@ -1,8 +1,33 @@
 <script lang="ts" setup>
 import './assets/css/tailwind.css';
+
+useMeta({
+  meta: [
+    { charset: 'utf-8' },
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    {
+      hid: 'description',
+      name: 'description',
+      content:
+        'フロントエンドエンジニアとしてのTipsやプログラミング初心者のためのノウハウを記事にしています。',
+    },
+    {
+      hid: 'og:site_name',
+      property: 'og:site_name',
+      content: 'Terakoya',
+    },
+    { hid: 'og:type', property: 'og:type', content: 'website' },
+  ],
+});
 </script>
 
 <template>
+  <Html lang="ja">
+    <Head>
+      <Title>Terakoya</Title>
+      <Link rel="icon" type="image/x-icon" href="/favicon.ico" />
+    </Head>
+  </Html>
   <div>
     <HeaderLayout />
     <NuxtPage />
